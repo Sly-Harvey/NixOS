14711de066a53b5ebbd775a238c084d20f4bf4a4
Harvey Jacobs-Grant <mheharvey@gmail.com>
2024-03-06 15:40:21 +0000

    use shell.nix when using install script

--- shell.nix
+++ shell.nix
@@ -2,9 +2,15 @@
 
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
+    git
     nix
     home-manager
-    git
+    figlet
+    lolcat
   ];
   NIX_CONFIG = "extra-experimental-features = nix-command flakes";
+  # shellHook =
+  # ''
+  #   echo "BUILDING!" | figlet -W | lolcat -F 0.3 -p 2.5 -S 300
+  # '';
 }
