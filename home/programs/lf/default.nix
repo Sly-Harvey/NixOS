c0df9eb186f1ce61ead0dc8c47e8d47a3266f28e
Harvey Jacobs-Grant <mheharvey@gmail.com>
2024-03-06 06:31:15 +0000

    added lf config

---
+++ home/programs/lf/default.nix
@@ -0,0 +1,7 @@
+{ config, pkgs, ... }:
+{
+  home.file.".config/lf/lfrc".source = ./lfrc;
+  home.file.".config/lf/lfcd.sh".source = ./lfcd.sh;
+  home.file.".config/lf/lf_kitty_clean".source = ./lf_kitty_clean;
+  home.file.".config/lf/lf_kitty_preview".source = ./lf_kitty_preview;
+}
