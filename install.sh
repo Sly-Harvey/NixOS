602c7cb6e7c0e56926c32518b10917c76331abeb
Harvey Jacobs-Grant <mheharvey@gmail.com>
2024-03-06 15:40:21 +0000

    use shell.nix when using install script

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 backupdir="/tmp/NixOS-backup"
-clear
 
 colorize_prompt() {
     local color="$1"
@@ -44,8 +43,6 @@ mkdir -p $backupdir/old &> /dev/null
 # Backup existing config
 mv /etc/nixos/* $backupdir/old &> /dev/null
 rm -r /etc/nixos/.* &> /dev/null
-# mv /etc/nixos/configuration.nix $backupdir/old
-# mv /etc/nixos/hardware-configuration.nix $backupdir/old
 
 # Clone github repo to /etc/nixos
 nix shell --experimental-features "nix-command flakes" nixpkgs#git --command git clone --branch hosts https://github.com/Sly-Harvey/NixOS.git /etc/nixos
@@ -57,12 +54,15 @@ ask_yes_no "Do you want to use current hardware-configuration.nix? (Recommended)
 
 if [ "$replaceHardwareConfig" == "Y" ]; then
     rm /etc/nixos/nixos/hosts/default.nix &> /dev/null
-    # mv /etc/nixos/nixos/hardware-configuration.nix $backupdir/new
     cp $(find $backupdir/old -iname 'hardware-configuration.nix') /etc/nixos/nixos/hosts/
     echo "{ imports = [ ./hardware-configuration.nix ]; }" >> /etc/nixos/nixos/hosts/default.nix
 fi
 
-nix run --experimental-features "nix-command flakes" nixpkgs#git -C /etc/nixos add *
-# git -C /etc/nixos add *
+nix-shell --command "git -C /etc/nixos add *"
+# nix shell --experimental-features "nix-command flakes" nixpkgs#git --command git -C /etc/nixos add *
 
-sudo nix shell --experimental-features "nix-command flakes" nixpkgs#git --command sudo nixos-rebuild switch --flake /etc/nixos#nixos
+clear
+nix-shell --command "echo BUILDING | figlet -cklnoW | lolcat -F 0.3 -p 2.5 -S 300"
+
+nix-shell --command "sudo nixos-rebuild switch --flake /etc/nixos#nixos"
+# sudo nix shell --experimental-features "nix-command flakes" nixpkgs#git --command sudo nixos-rebuild switch --flake /etc/nixos#nixos
