82bfc4c3ff15b40bb44905e9ed355005d452ed83
Harvey Jacobs-Grant <mheharvey@gmail.com>
2024-03-09 04:46:17 +0000

    updated nvim

--- home/programs/nvim/default.nix
+++ home/programs/nvim/default.nix
@@ -5,7 +5,8 @@
     home.file.".config/nvim" = {
         source = builtins.fetchGit {
             url = "https://github.com/Sly-Harvey/nvim.git";
-            rev = "afd114996c8c1713d5e06ba32569b544b2929dd6";
+            ref = "performance";
+            rev = "c2d87c81515261e7e3b0040e2e4b3a7b0a8c0544";
         };
         recursive = true;
     };
