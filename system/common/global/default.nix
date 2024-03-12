416053bafc8a31e08dc6f115ec2ad4f61110d18e
Sly-Harvey <mheharvey@gmail.com>
2024-03-12 13:22:20 +0000

    updated .gitignore and added cachix

--- system/common/global/default.nix
+++ system/common/global/default.nix
@@ -181,6 +181,14 @@
     # Nix Package Manager Settings
     settings = {
       #auto-optimise-store = true; # Disbabled for now because it takes a long time to build
+      substituters = [
+        "https://cache.nixos.org/"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
       experimental-features = ["nix-command" "flakes"];
       warn-dirty = false;
       keep-outputs = true;
