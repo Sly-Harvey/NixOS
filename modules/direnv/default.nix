a3f97932eb6f2144ca6f9e2a4f1f79e547eb6a51
Sly-Harvey <mheharvey@gmail.com>
2024-03-15 05:31:24 +0000

    Modules!

---
+++ modules/direnv/default.nix
@@ -0,0 +1,16 @@
+{
+  home-manager,
+  username,
+  ...
+}: {
+  home-manager.users.${username} = _: {
+    programs.direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+      enableFishIntegration = true;
+      enableNushellIntegration = true;
+    };
+  };
+}
