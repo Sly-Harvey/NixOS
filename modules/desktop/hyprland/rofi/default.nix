adcbcd59cc9a40d4e55e664d4a996b0f9fbd2274
Sly-Harvey <mheharvey@gmail.com>
2024-03-18 01:06:09 +0000

    fixed rofi not focusing

--- modules/desktop/hyprland/rofi/default.nix
+++ modules/desktop/hyprland/rofi/default.nix
@@ -4,12 +4,9 @@
   ...
 }: {
   home-manager.users.${username} = _: {
-    programs.rofi = {
-      enable = true;
-      terminal = "${pkgs.cool-retro-term}/bin/cool-retro-term";
-      # theme = ./launchers/type-2/style-2.rasi;
-      #theme = ./launchers/type-2/style-12.rasi;
-    };
+    home.packages = with pkgs; [
+      rofi-wayland
+    ];
     home.file.".config/rofi/config-music.rasi".source = ./config-music.rasi;
     home.file.".config/rofi/config-long.rasi".source = ./config-long.rasi;
     home.file.".config/rofi/config-wallpaper.rasi".source = ./config-wallpaper.rasi;
