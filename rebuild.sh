dc479db9d9aecef5c885c707066cfc0ab4a880cb
Sly-Harvey <86175780+Sly-Harvey@users.noreply.github.com>
2024-03-27 03:27:47 +0000

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 scriptdir=$(realpath $(dirname $0))
 currentUser=$(logname)
-sed -i -e 's/user = \".*\"/user = \"'$currentUser'\"/' $scriptdir/flake.nix
+sed -i -e 's/username = \".*\"/username = \"'$currentUser'\"/' $scriptdir/flake.nix
 sudo nixos-rebuild switch --flake .#nixos
