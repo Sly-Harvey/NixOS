aa81b3008face377a50e64e5e0205bf1d46a14e2
Sly-Harvey <mheharvey@gmail.com>
2024-03-31 06:21:57 +0100

    fixed wlsunset

--- modules/desktop/hyprland/hypr/hyprland-environment.nix
+++ modules/desktop/hyprland/hypr/hyprland-environment.nix
@@ -8,15 +8,17 @@
 
       WLR_NO_HARDWARE_CURSORS = "1";
       WLR_RENDERER_ALLOW_SOFTWARE = "1";
-      WLR_RENDERER = "vulkan";
+      # WLR_RENDERER = "vulkan";
 
-      GBM_BACKEND = "nvidia-drm";
-      LIBVA_DRIVER_NAME = "nvidia"; # hardware acceleration
-      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+      # GBM_BACKEND = "nvidia-drm";
+      # LIBVA_DRIVER_NAME = "nvidia"; # hardware acceleration
+      # __GLX_VENDOR_LIBRARY_NAME = "nvidia";
 
       CLUTTER_BACKEND = "wayland";
       __GL_VRR_ALLOWED = "1";
 
+      QT_QPA_PLATFORM = "wayland";
+      SDL_VIDEODRIVER = "wayland";
       XDG_CURRENT_DESKTOP = "Hyprland";
       XDG_SESSION_DESKTOP = "Hyprland";
       XDG_SESSION_TYPE = "wayland";
