d2052b2d72c830b9cd2b7a3e78f3813b7ac061b6
Sly-Harvey <mheharvey@gmail.com>
2024-04-06 01:39:04 +0100

    improved firefox config

--- modules/programs/firefox/default.nix
+++ modules/programs/firefox/default.nix
@@ -12,6 +12,72 @@
     Value = true;
     Status = "locked";
   };
+  settings = {
+    # Performance settings
+    "gfx.webrender.all" = true; # Force enable GPU acceleration
+    "media.ffmpeg.vaapi.enabled" = true;
+    "widget.dmabuf.force-enabled" = true; # Required in recent Firefoxes
+    "reader.parse-on-load.force-enabled" = true;
+    "privacy.webrtc.legacyGlobalIndicator" = false;
+
+    # Use cloudflare for better security/privacy
+    "network.trr.mode" = 3; # 2
+    "network.trr.custom_uri" = "1.1.1.1";
+    "network.trr.uri" = "1.1.1.1";
+
+    # Remove trackers
+    "privacy.purge_trackers.enabled" = true;
+    "privacy.trackingprotection.enabled" = true;
+    "privacy.trackingprotection.socialtracking.enabled" = true;
+    "privacy.donottrackheader.enabled" = true;
+    "privacy.donottrackheader.value" = 1;
+
+    # Block more unwanted stuff
+    "browser.contentblocking.category" = "strict";
+    "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
+    "browser.shell.checkDefaultBrowser" = false;
+    "signon.rememberSignons" = false;
+
+    # General settings
+    "ui.key.accelKey" = 17; # Set CTRL as master key
+    "browser.newtabpage.pinned" = true; # false
+    "browser.bookmarks.defaultLocation" = "toolbar";
+    "browser.startup.page" = 3;
+    "browser.newtab.url" = "about:blank";
+    "browser.newtabpage.activity-stream.showSponsored" = false;
+    "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+    "app.shield.optoutstudies.enabled" = false;
+    "dom.security.https_only_mode" = true;
+    "dom.security.https_only_mode_ever_enabled" = true;
+    "identity.fxaccounts.enabled" = false;
+    "app.update.auto" = false;
+    "browser.startup.homepage" = "";
+    "browser.bookmarks.restore_default_bookmarks" = false;
+    "browser.ctrlTab.recentlyUsedOrder" = false;
+    "browser.discovery.enabled" = false;
+    "browser.laterrun.enabled" = false;
+    "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons" = false;
+    "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features" = false;
+    "browser.newtabpage.activity-stream.feeds.snippets" = false;
+    "browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.havePinned" = "";
+    "browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.searchEngines" = "";
+    "browser.protections_panel.infoMessage.seen" = true;
+    "browser.ssb.enabled" = true;
+    "browser.toolbars.bookmarks.visibility" = "newtab";
+    #"browser.urlbar.placeholderName" = "Google";
+    "browser.urlbar.suggest.openpage" = false;
+    "datareporting.policy.dataSubmissionEnable" = false;
+    "datareporting.policy.dataSubmissionPolicyAcceptedVersion" = 2;
+
+    "extensions.getAddons.showPane" = false;
+    "extensions.htmlaboutaddons.recommendations.enabled" = false;
+    "extensions.extensions.activeThemeID" = "firefox-alpenglow@mozilla.org";
+    "extensions.update.enabled" = false;
+    "extensions.webcompat.enable_picture_in_picture_overrides" = true;
+    "extensions.webcompat.enable_shims" = true;
+    "extensions.webcompat.perform_injections" = true;
+    "extensions.webcompat.perform_ua_overrides" = true;
+  };
 in {
   home-manager.users.${username} = _: {
     programs = {
@@ -45,7 +111,7 @@ in {
               # };
               "{c2c003ee-bd69-42a2-b0e9-6f34222cb046}" = {
                 install_url = "https://addons.mozilla.org/firefox/downloads/latest/auto-tab-discard/latest.xpi";
-                installation_mode = "force_installed";
+                installation_mode = "normal_installed";
               };
               "queryamoid@kaply.com" = {
                 install_url = "https://github.com/mkaply/queryamoid/releases/download/v0.1/query_amo_addon_id-0.1-fx.xpi";
@@ -83,26 +149,62 @@ in {
             name = "default"; # name as listed in about:profiles
             isDefault = true; # can be omitted; true if profile ID is 0
             extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-              #profile-switcher
+              # profile-switcher
             ];
             bookmarks = [
               {
-                name = "Youtube";
-                url = "https://www.youtube.com/";
-              }
-              {
-                name = "Github";
-                url = "https://github.com/";
-              }
-              {
-                name = "Firefox addons";
-                url = "https://gitlab.com/rycee/nur-expressions/-/blob/master/pkgs/firefox-addons/addons.json";
+                name = "Bookmarks Toolbar";
+                toolbar = true;
+                bookmarks = [
+                  {
+                    name = "Youtube";
+                    url = "https://www.youtube.com/";
+                  }
+                  {
+                    name = "Github";
+                    url = "https://github.com/";
+                  }
+                  {
+                    name = "NixOS pkgs";
+                    url = "https://search.nixos.org/packages";
+                  }
+                  {
+                    name = "NixOS Wiki";
+                    url = "https://nixos.wiki";
+                  }
+                  {
+                    name = "NixOS Configs";
+                    url = "https://nixos.wiki/wiki/Configuration_Collection";
+                  }
+                  {
+                    name = "Search Engines";
+                    bookmarks = [
+                      {
+                        name = "Startpage";
+                        url = "https://www.startpage.com/sp/search?query={searchTerms}&prfe=dea8b8a2e1126185da987128a196ee5c47cdf324dce146f96b3b9157ab1f9e7166ae05d134c935eccc20f54e46222c8f1bb60faece00557b02e7a4e1fe397bc0f6750fbd3f7f580b241188&abp=-1";
+                      }
+                      {
+                        name = "SearX";
+                        url = "https://searx.aicampground.com";
+                      }
+                    ];
+                  }
+                ];
               }
             ];
             search = {
               force = true;
               default = "Google";
-              order = ["Google" "Startpage" "Searx"];
+              privateDefault = "Startpage";
+              order = [
+                "Startpage"
+                "NixOS Packages"
+                "NixOS Options"
+                "NixOS Wiki"
+                "Home Manager Options"
+                "Searx"
+                "Google"
+              ];
               engines = {
                 "Startpage" = {
                   urls = [
@@ -111,9 +213,9 @@ in {
                     }
                   ];
                   icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-                  definedAliases = ["@startpage"];
+                  definedAliases = ["@sp"];
                 };
-                "Nix Packages" = {
+                "NixOS Packages" = {
                   urls = [
                     {
                       template = "https://search.nixos.org/packages";
@@ -132,145 +234,10 @@ in {
                   icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
                   definedAliases = ["@np"];
                 };
-                "NixOS Wiki" = {
-                  urls = [{template = "https://nixos.wiki/index.php?search={searchTerms}";}];
-                  iconUpdateURL = "https://nixos.wiki/favicon.png";
-                  updateInterval = 24 * 60 * 60 * 1000; # every day
-                  definedAliases = ["@nw"];
-                };
-                "Searx" = {
-                  urls = [{template = "https://searx.aicampground.com/?q={searchTerms}";}];
-                  iconUpdateURL = "https://nixos.wiki/favicon.png";
-                  updateInterval = 24 * 60 * 60 * 1000; # every day
-                  definedAliases = ["@searx"];
-                };
-                "Bing".metaData.hidden = true;
-                "Google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias
-              };
-            };
-            settings = {
-              # Performance settings
-              "gfx.webrender.all" = true; # Force enable GPU acceleration
-              "media.ffmpeg.vaapi.enabled" = true;
-              "widget.dmabuf.force-enabled" = true; # Required in recent Firefoxes
-
-              "ui.key.accelKey" = 17;
-
-              # Keep the reader button enabled at all times; really don't
-              # care if it doesn't work 20% of the time, most websites are
-              # crap and unreadable without this
-              "reader.parse-on-load.force-enabled" = true;
-
-              # Hide the "sharing indicator", it's especially annoying
-              # with tiling WMs on wayland
-              "privacy.webrtc.legacyGlobalIndicator" = false;
-
-              # Actual settings
-              "app.update.auto" = false;
-              "browser.startup.homepage" = "";
-              "browser.bookmarks.restore_default_bookmarks" = false;
-              "browser.ctrlTab.recentlyUsedOrder" = false;
-              "browser.discovery.enabled" = false;
-              "browser.laterrun.enabled" = false;
-              "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons" = false;
-              "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features" = false;
-              "browser.newtabpage.activity-stream.feeds.snippets" = false;
-              "browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.havePinned" = "";
-              "browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.searchEngines" = "";
-              "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
-
-              "browser.newtabpage.pinned" = false;
-              "browser.protections_panel.infoMessage.seen" = true;
-
-              "browser.ssb.enabled" = true;
-              "browser.toolbars.bookmarks.visibility" = "newtab";
-              #"browser.urlbar.placeholderName" = "Google";
-              "browser.urlbar.suggest.openpage" = false;
-              "datareporting.policy.dataSubmissionEnable" = false;
-              "datareporting.policy.dataSubmissionPolicyAcceptedVersion" = 2;
-
-              "extensions.getAddons.showPane" = false;
-              "extensions.htmlaboutaddons.recommendations.enabled" = false;
-              "extensions.extensions.activeThemeID" = "firefox-alpenglow@mozilla.org";
-              "extensions.update.enabled" = false;
-              "extensions.webcompat.enable_picture_in_picture_overrides" = true;
-              "extensions.webcompat.enable_shims" = true;
-              "extensions.webcompat.perform_injections" = true;
-              "extensions.webcompat.perform_ua_overrides" = true;
-
-              "network.trr.mode" = 2;
-              "network.trr.custom_uri" = "1.1.1.1";
-              "network.trr.uri" = "1.1.1.1";
-
-              "signon.rememberSignons" = false;
-              "browser.startup.page" = 3;
-              "browser.newtabpage.activity-stream.showSponsored" = false;
-              "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
-              "app.shield.optoutstudies.enabled" = false;
-              "browser.contentblocking.category" = "strict";
-              "browser.shell.checkDefaultBrowser" = false;
-              "dom.security.https_only_mode" = true;
-              "dom.security.https_only_mode_ever_enabled" = true;
-              "identity.fxaccounts.enabled" = false;
-              "privacy.trackingprotection.enabled" = true;
-              "privacy.trackingprotection.socialtracking.enabled" = true;
-              "privacy.donottrackheader.enabled" = true;
-            };
-            extraConfig = ''
-              lockPref("extensions.autoDisableScopes", 0);
-            '';
-          };
-          private = {
-            id = 1;
-            name = "private";
-            isDefault = false;
-            extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
-              ublock-origin
-              privacy-badger
-              darkreader
-              profile-switcher
-              vimium
-            ];
-            bookmarks = [
-              {
-                name = "Youtube";
-                url = "https://www.youtube.com/";
-              }
-              {
-                name = "Twitch";
-                url = "https://www.twitch.tv/";
-              }
-              {
-                name = "Kick";
-                url = "https://www.kick.com/";
-              }
-              {
-                name = "Github";
-                url = "https://github.com/";
-              }
-              {
-                name = "Firefox addons";
-                url = "https://gitlab.com/rycee/nur-expressions/-/blob/master/pkgs/firefox-addons/addons.json";
-              }
-            ];
-            search = {
-              force = true;
-              default = "Startpage";
-              order = ["Starpage" "Searx" "Google"];
-              engines = {
-                "Startpage" = {
+                "NixOS Options" = {
                   urls = [
                     {
-                      template = "https://www.startpage.com/sp/search?query={searchTerms}&prfe=dea8b8a2e1126185da987128a196ee5c47cdf324dce146f96b3b9157ab1f9e7166ae05d134c935eccc20f54e46222c8f1bb60faece00557b02e7a4e1fe397bc0f6750fbd3f7f580b241188&abp=-1";
-                    }
-                  ];
-                  icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-                  definedAliases = ["@startpage"];
-                };
-                "Nix Packages" = {
-                  urls = [
-                    {
-                      template = "https://search.nixos.org/packages";
+                      template = "https://search.nixos.org/options";
                       params = [
                         {
                           name = "type";
@@ -283,8 +250,8 @@ in {
                       ];
                     }
                   ];
-                  icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-                  definedAliases = ["@np"];
+                  icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                  definedAliases = ["@no"];
                 };
                 "NixOS Wiki" = {
                   urls = [{template = "https://nixos.wiki/index.php?search={searchTerms}";}];
@@ -292,85 +259,34 @@ in {
                   updateInterval = 24 * 60 * 60 * 1000; # every day
                   definedAliases = ["@nw"];
                 };
+                "Home Manager Options" = {
+                  urls = [
+                    {
+                      template = "https://mipmip.github.io/home-manager-option-search";
+                      params = [
+                        {
+                          name = "query";
+                          value = "{searchTerms}";
+                        }
+                      ];
+                    }
+                  ];
+                  iconUpdateURL = "https://avatars.githubusercontent.com/u/33221035";
+                  updateInterval = 24 * 60 * 60 * 1000; # Update every day.
+                  definedAliases = ["@hm"];
+                };
                 "Searx" = {
                   urls = [{template = "https://searx.aicampground.com/?q={searchTerms}";}];
                   iconUpdateURL = "https://nixos.wiki/favicon.png";
                   updateInterval = 24 * 60 * 60 * 1000; # every day
-                  definedAliases = ["@searx"];
+                  definedAliases = ["@sx"];
                 };
                 "Bing".metaData.hidden = true;
+                "Ebay".metaData.hidden = true;
                 "Google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias
               };
             };
-            settings = {
-              # Performance settings
-              "gfx.webrender.all" = true; # Force enable GPU acceleration
-              "media.ffmpeg.vaapi.enabled" = true;
-              "widget.dmabuf.force-enabled" = true; # Required in recent Firefoxes
-
-              "ui.key.accelKey" = 17;
-
-              # Keep the reader button enabled at all times; really don't
-              # care if it doesn't work 20% of the time, most websites are
-              # crap and unreadable without this
-              "reader.parse-on-load.force-enabled" = true;
-
-              # Hide the "sharing indicator", it's especially annoying
-              # with tiling WMs on wayland
-              "privacy.webrtc.legacyGlobalIndicator" = false;
-
-              # Actual settings
-              "app.update.auto" = false;
-              "browser.startup.homepage" = "";
-              "browser.bookmarks.restore_default_bookmarks" = false;
-              "browser.ctrlTab.recentlyUsedOrder" = false;
-              "browser.discovery.enabled" = false;
-              "browser.laterrun.enabled" = false;
-              "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons" = false;
-              "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features" = false;
-              "browser.newtabpage.activity-stream.feeds.snippets" = false;
-              "browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.havePinned" = "";
-              "browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.searchEngines" = "";
-              "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
-
-              "browser.newtabpage.pinned" = false;
-              "browser.protections_panel.infoMessage.seen" = true;
-              "browser.quitShortcut.disabled" = true;
-
-              "browser.ssb.enabled" = true;
-              "browser.toolbars.bookmarks.visibility" = "never";
-              #"browser.urlbar.placeholderName" = "Startpage";
-              "browser.urlbar.suggest.openpage" = false;
-              "datareporting.policy.dataSubmissionEnable" = false;
-              "datareporting.policy.dataSubmissionPolicyAcceptedVersion" = 2;
-
-              "extensions.getAddons.showPane" = false;
-              "extensions.htmlaboutaddons.recommendations.enabled" = false;
-              "extensions.extensions.activeThemeID" = "firefox-alpenglow@mozilla.org";
-              "extensions.update.enabled" = false;
-              "extensions.webcompat.enable_picture_in_picture_overrides" = true;
-              "extensions.webcompat.enable_shims" = true;
-              "extensions.webcompat.perform_injections" = true;
-              "extensions.webcompat.perform_ua_overrides" = true;
-
-              "network.trr.mode" = 2;
-              "network.trr.custom_uri" = "1.1.1.1";
-              "network.trr.uri" = "1.1.1.1";
-
-              "signon.rememberSignons" = false;
-              "browser.startup.page" = 0;
-              "browser.newtabpage.activity-stream.showSponsored" = false;
-              "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
-              "app.shield.optoutstudies.enabled" = false;
-              "browser.contentblocking.category" = "strict";
-              "browser.shell.checkDefaultBrowser" = false;
-              "dom.security.https_only_mode" = true;
-              "dom.security.https_only_mode_ever_enabled" = true;
-              "identity.fxaccounts.enabled" = false;
-              "privacy.trackingprotection.enabled" = true;
-              "privacy.trackingprotection.socialtracking.enabled" = true;
-              "privacy.donottrackheader.enabled" = true;
-            };
+            settings = settings;
             extraConfig = ''
               lockPref("extensions.autoDisableScopes", 0);
             '';
