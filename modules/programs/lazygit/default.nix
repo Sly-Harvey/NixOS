8b9ffe3c9303fb81e5fbe89a9dcf5f4ff682e9d3
Sly-Harvey <mheharvey@gmail.com>
2024-04-15 09:10:17 +0100

    added lazygit

---
+++ modules/programs/lazygit/default.nix
@@ -0,0 +1,34 @@
+{
+  pkgs,
+  config,
+  lib,
+  username,
+  ...
+}: let
+  fromYAML = f: let
+    jsonFile =
+      pkgs.runCommand "lazygit yaml to attribute set" {nativeBuildInputs = [pkgs.jc];} # bash
+      
+      ''
+        jc --yaml < "${f}" > "$out"
+      '';
+  in
+    builtins.elemAt (builtins.fromJSON (builtins.readFile jsonFile)) 0;
+in {
+  home-manager.users.${username} = _: {
+    home.shellAliases = {
+      lg = "lazygit";
+    };
+    programs.lazygit = {
+      enable = true;
+      settings = {
+        gui = fromYAML (
+          pkgs.catppuccin + "/lazygit/themes/blue.yml"
+        );
+        git = {
+          overrideGpg = true;
+        };
+      };
+    };
+  };
+}
