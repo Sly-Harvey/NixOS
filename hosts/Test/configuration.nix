753b2a2494bddb7f6c5146e135099457971d9ebf
Sly-Harvey <86175780+Sly-Harvey@users.noreply.github.com>
2024-04-17 07:44:37 +0100

    added test config
    changed workflow
    
    added test config
    
    Create main.yml

---
+++ hosts/Test/configuration.nix
@@ -0,0 +1,104 @@
+# Please Don't use this test config since it disables drivers.
+# And has a different hardware config.
+# It's not meant to be used for Personal use.
+# This is for automatic testing with github actions.
+
+{
+  pkgs,
+  username,
+  locale,
+  timezone,
+  ...
+}: {
+  imports = [
+    ../common.nix
+    ../../modules/desktop/hyprland # Enable Hyprland window manager
+    ./hardware-configuration.nix
+  ];
+
+  # Home-manager config
+  home-manager.users.${username} = {
+    home.username = username;
+    home.homeDirectory = "/home/${username}";
+
+    home.stateVersion = "23.11"; # Please read the comment before changing.
+
+    home.packages = with pkgs; [
+      #vim
+      #krita
+      #steam
+      (pkgs.writeShellScriptBin "hello" ''
+        echo "Hello ''${username}!"
+      '')
+    ];
+
+    home.sessionVariables = {
+      # EDITOR = "emacs";
+    };
+
+    # Let Home Manager install and manage itself.
+    programs.home-manager.enable = true;
+  };
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Timezone and locale
+  time.timeZone = timezone;
+  i18n.defaultLocale = locale;
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = locale;
+    LC_IDENTIFICATION = locale;
+    LC_MEASUREMENT = locale;
+    LC_MONETARY = locale;
+    LC_NAME = locale;
+    LC_NUMERIC = locale;
+    LC_PAPER = locale;
+    LC_TELEPHONE = locale;
+    LC_TIME = locale;
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "gb";
+    variant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "uk";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.${username} = {
+    isNormalUser = true;
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "video"
+      "audio"
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+  ];
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+}
--- rebuild.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-scriptdir=$(realpath $(dirname $0))
-currentUser=$(logname)
-sed -i -e 's/username = \".*\"/username = \"'$currentUser'\"/' $scriptdir/flake.nix
-sudo nixos-rebuild switch --flake .#nixos
