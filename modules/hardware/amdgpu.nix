1763371397562e964320dcb30a67dcdd8e354312
Sly-Harvey <mheharvey@gmail.com>
2024-05-30 07:24:20 +0100

    Properly configured amdgpu drivers

--- modules/hardware/amdgpu.nix
+++ modules/hardware/amdgpu.nix
@@ -1,15 +1,27 @@
 # This module is untested since i don't own an amd gpu!
-{
-  pkgs,
-  ...
-}: {
-  boot.kernelModules = ["amdgpu" "radeon"];
-  boot.blacklistedKernelModules = ["fglrx"];
-  boot.kernelParams = ["radeon.si_support=0 radeon.cik_support=0" "amdgpu.si_support=1 amdgpu.cik_support=1"];
+{pkgs, ...}: {
+  boot = {
+    initrd.kernelModules = ["amdgpu"]; # radeon etc.
+    # blacklistedKernelModules = ["fglrx"];
+    # kernelParams = ["radeon.cik_support=0" "amdgpu.cik_support=1"]; # for Sea Islands (CIK i.e. GCN 2) cards
+    # kernelParams = ["radeon.si_support=0" "amdgpu.si_support=1"]; # for Southern Islands (SI i.e. GCN 1) cards
+  };
+  services.xserver = {
+    enable = true;
+    videoDrivers = ["amdgpu"];
+  };
   hardware.opengl = {
     enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
     extraPackages = with pkgs; [
-      # Add any additional packages needed for specific AMD features or utilities
+      amdvlk
+      libvdpau-va-gl
+      vaapiVdpau
+      vdpauinfo
+      rocm-opencl-icd
+      rocm-opencl-runtime
     ];
+    extraPackages32 = with pkgs; [driversi686Linux.amdvlk];
   };
 }
