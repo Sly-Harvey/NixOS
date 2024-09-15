837e9fac73e7c96c4b99d52dc1e6bf4401bb0ca0
Sly-Harvey <mheharvey@gmail.com>
2024-09-15 05:25:54 +0100

    fixed mpv not playing obs videos

--- hosts/common.nix
+++ hosts/common.nix
@@ -117,7 +117,7 @@ in {
 
   xdg.portal = {
     enable = true;
-    configPackages = with pkgs; [xdg-desktop-portal-gtk];
+    extraPortals = with pkgs; [xdg-desktop-portal-gtk];
   };
 
   # Enable dconf for home-manager
@@ -250,13 +250,11 @@ in {
       substituters = [
         "https://cache.nixos.org/"
         "https://nix-community.cachix.org/"
-        "https://hyprland.cachix.org/"
         "https://nix-gaming.cachix.org/"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       ];
       experimental-features = ["nix-command" "flakes" "repl-flake"];
--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -16,6 +16,11 @@ in {
     ./programs/swaync
   ];
 
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org/"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
+
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
