d7b0107e0d414442130c1e9c970e15330aaed88d
Sly-Harvey <mheharvey@gmail.com>
2024-12-13 11:24:57 +0000

    fixed word splitting in tmux-sessionizer

--- modules/scripts/tmux-sessionizer.nix
+++ modules/scripts/tmux-sessionizer.nix
@@ -3,12 +3,12 @@ pkgs.writeShellScriptBin "tmux-sessionizer" ''
   tmux="${pkgs.tmux}/bin/tmux"
   fzf="${pkgs.fzf}/bin/fzf"
   if [[ $# -eq 1 ]]; then
-      selected=$1
+      selected="$1"
   else
       selected=$(find ~/ ~/git-clone/ /mnt/work/Projects/ -mindepth 1 -maxdepth 1 -type d | $fzf)
   fi
 
-  if [[ -z $selected ]]; then
+  if [[ -z "$selected" ]]; then
       exit 0
   fi
 
@@ -18,15 +18,15 @@ pkgs.writeShellScriptBin "tmux-sessionizer" ''
 
   tmux_running=$(pgrep tmux)
 
-  if [[ -z $TMUX ]] || [[ -z $tmux_running ]]; then
-      $tmux new-session -A -s $selected_name -c $selected
+  if [[ -z "$TMUX" ]] || [[ -z "$tmux_running" ]]; then
+      $tmux new-session -A -s "$selected_name" -c "$selected"
       exit 0
   fi
 
-  if ! $tmux has-session -t=$selected_name 2> /dev/null; then
-      $tmux new-session -ds $selected_name -c $selected
+  if ! $tmux has-session -t="$selected_name" 2> /dev/null; then
+      $tmux new-session -ds "$selected_name" -c "$selected"
   fi
 
-  $tmux switch-client -t $selected_name
-  # $tmux attach-session -t $selected_name
+  $tmux switch-client -t "$selected_name"
+  # $tmux attach-session -t "$selected_name"
 ''
