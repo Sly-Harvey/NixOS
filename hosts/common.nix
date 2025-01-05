873bc94ec8e453a2f6702fe945cddb9ef6dc575b
Sly-Harvey <mheharvey@gmail.com>
2025-01-05 02:23:18 +0000

    Updated flake

--- hosts/common.nix
+++ hosts/common.nix
@@ -205,7 +205,7 @@ in {
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
