0f09bd4086ae4c8d47bb574a69ec921cbdbf2d24
Sly-Harvey <mheharvey@gmail.com>
2025-01-07 03:26:56 +0000

    Use catppuccin themFile

--- modules/programs/terminal/kitty/default.nix
+++ modules/programs/terminal/kitty/default.nix
@@ -7,7 +7,7 @@
           name = "JetBrainsMono Nerd Font";
           size = 12.0;
         };
-        # themeFile = "Catppuccin-Mocha";
+        themeFile = "Catppuccin-Mocha";
         settings = {
           # shell = "${getExe pkgs.tmux}";
           term = "xterm-256color";
@@ -30,51 +30,10 @@
           inactive_tab_font_style = "normal";
           tab_bar_style = "powerline";
           tab_powerline_style = "round";
-          # active_tab_foreground = "#1e1e2e";
-          # active_tab_background = "#cba6f7";
-          # inactive_tab_foreground = "#bac2de";
-          # inactive_tab_background = "#313244";
-
-          ## Catppuccin-Mocha
-          foreground = "#CDD6F4";
-          background = "#1E1E2E";
-          selection_foreground = "#1E1E2E";
-          selection_background = "#F5E0DC";
-          cursor = "#F5E0DC";
-          cursor_text_color = "#1E1E2E";
-          url_color = "#F5E0DC";
-          active_border_color = "#B4BEFE";
-          inactive_border_color = "#6C7086";
-          bell_border_color = "#F9E2AF";
-          wayland_titlebar_color = "system";
-          macos_titlebar_color = "system";
-          active_tab_foreground = "#11111B";
-          active_tab_background = "#CBA6F7";
-          inactive_tab_foreground = "#CDD6F4";
-          inactive_tab_background = "#181825";
-          tab_bar_background = "#11111B";
-          mark1_foreground = "#1E1E2E";
-          mark1_background = "#B4BEFE";
-          mark2_foreground = "#1E1E2E";
-          mark2_background = "#CBA6F7";
-          mark3_foreground = "#1E1E2E";
-          mark3_background = "#74C7EC";
-          color0 = "#45475A";
-          color1 = "#F38BA8";
-          color2 = "#A6E3A1";
-          color3 = "#F9E2AF";
-          color4 = "#89B4FA";
-          color5 = "#F5C2E7";
-          color6 = "#94E2D5";
-          color7 = "#BAC2DE";
-          color8 = "#585B70";
-          color9 = "#F38BA8";
-          color10 = "#A6E3A1";
-          color11 = "#F9E2AF";
-          color12 = "#89B4FA";
-          color13 = "#F5C2E7";
-          color14 = "#94E2D5";
-          color15 = "#A6ADC8";
+          active_tab_foreground = "#1e1e2e";
+          active_tab_background = "#cba6f7";
+          inactive_tab_foreground = "#bac2de";
+          inactive_tab_background = "#313244";
         };
         # shellIntegration.mode = "no-sudo";
         keybindings = {
