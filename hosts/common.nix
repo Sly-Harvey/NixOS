cb109ebdfbd31f11c3b8fafbadc5e2e79530fc48
Sly-Harvey <mheharvey@gmail.com>
2025-02-13 05:54:25 +0000

    Updated flake

--- hosts/common.nix
+++ hosts/common.nix
@@ -105,8 +105,8 @@ in {
   # Bootloader.
   boot = {
     tmp.cleanOnBoot = true;
-    kernelPackages = pkgs.linuxPackages_latest; # _latest, _zen, _xanmod_latest, _hardened, _rt, _OTHER_CHANNEL, etc.
-    # kernelPackages = pkgs.linuxKernel.packages.linux_6_12;
+    # kernelPackages = pkgs.linuxPackages_latest; # _latest, _zen, _xanmod_latest, _hardened, _rt, _OTHER_CHANNEL, etc.
+    kernelPackages = pkgs.linuxKernel.packages.linux_6_12;
     loader = {
       efi.canTouchEfiVariables = true;
       efi.efiSysMountPoint = "/boot";
