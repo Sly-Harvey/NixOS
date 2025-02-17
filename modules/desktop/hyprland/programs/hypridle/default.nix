91983a8a8674a71e76ca7f2e84c100f6fd324e19
Sly-Harvey <86175780+Sly-Harvey@users.noreply.github.com>
2025-02-17 02:04:56 +0000

    Added hypridle
    Disabled auto dpms

---
+++ modules/desktop/hyprland/programs/hypridle/default.nix
@@ -0,0 +1,33 @@
+{...}: {
+  home-manager.sharedModules = [
+    (_: {
+      services.hypridle = {
+        enable = true;
+        settings = {
+          general = {
+            ignore_dbus_inhibit = false;
+            lock_cmd = "pidof hyprlock || hyprlock";
+            unlock_cmd = "pkill --signal SIGUSR1 hyprlock";
+            before_sleep_cmd = "loginctl lock-session";
+            after_sleep_cmd = "hyprctl dispatch dpms on";
+          };
+          listener = [
+            {
+              timeout = 300; # 5 Minutes
+              on-timeout = "loginctl lock-session";
+            }
+            /* {
+              timeout = 360; # 6 Minutes
+              on-timeout = "hyprctl dispatch dpms off";
+              on-resume = "hyprctl dispatch dpms on";
+            } */
+            /* {
+              timeout = 600; # 10m
+              on-timeout = "systemctl suspend";
+            } */
+          ];
+        };
+      };
+    })
+  ];
+}
