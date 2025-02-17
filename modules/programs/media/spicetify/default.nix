fdac63a9bded4f45d6d1ec27cdc374ef01588149
Sly-Harvey <86175780+Sly-Harvey@users.noreply.github.com>
2025-02-17 02:52:44 +0000

    Updated flake

--- modules/programs/media/spicetify/default.nix
+++ modules/programs/media/spicetify/default.nix
@@ -1,18 +1,16 @@
-{spicetify-nix, ...}: {
+{spicetify-nix, lib, ...}: {
+  # allow spotify to be installed if you don't have unfree enabled already
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "spotify"
+    ];
   home-manager.sharedModules = [
     ({
       pkgs,
-      lib,
       ...
     }: let
       spicePkgs = spicetify-nix.legacyPackages.${pkgs.system};
     in {
-      # allow spotify to be installed if you don't have unfree enabled already
-      nixpkgs.config.allowUnfreePredicate = pkg:
-        builtins.elem (lib.getName pkg) [
-          "spotify"
-        ];
-
       # import the flake's module for your system
       imports = [spicetify-nix.homeManagerModules.default];
 
