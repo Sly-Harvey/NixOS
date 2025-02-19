2e9f151aca85408a1ab4a3881cba00942f7dc246
ChrolloRifat <officialmhrifat@outlook.com>
2025-02-19 18:14:36 +0600

    Add thunar module and thumbnail support

---
+++ modules/programs/misc/thunar/default.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+
+{
+  programs.thunar = {
+    enable = true;
+    plugins = with pkgs.xfce; [
+      thunar-archive-plugin
+      thunar-volman
+    ];
+  };
+
+  services.tumbler.enable = true;        # Thumbnail support for images
+}
