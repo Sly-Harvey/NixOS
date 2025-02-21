0c636c80c85641a157468721d4f7c0a2e07f311c
Sly-Harvey <86175780+Sly-Harvey@users.noreply.github.com>
2025-02-21 17:23:45 +0000

    Styled wezterm

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
       username = "error"; # no need to touch this since install.sh uses sed to replace this (otherwise if manually installing then you need to change this yourself)
       editor = "nixvim"; # nixvim, vscode, emacs, nvchad, neovim
       browser = "firefox"; # firefox, zen-browser
-      terminal = "wezterm"; # kitty or alacritty or wezterm
+      terminal = "kitty"; # kitty, alacritty, wezterm
       terminalFileManager = "yazi"; # yazi or lf
       wallpaper = "Train.jpg"; # see modules/themes/wallpapers
 
