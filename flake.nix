b711a65db4f82c455217bcb01804b797461ee043
Sly-Harvey <harveyjacobs64@gmail.com>
2025-02-24 03:16:19 +0000

    added comma and nix-index-database

--- flake.nix
+++ flake.nix
@@ -8,6 +8,10 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixvim = {
       url = "github:Sly-Harvey/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
