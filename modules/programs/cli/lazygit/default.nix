64bf472d69e28e6ead20a88fd4c6288384e17900
Sly-Harvey <harveyjacobs64@gmail.com>
2025-02-26 06:51:12 +0000

    Updated flake

--- modules/programs/cli/lazygit/default.nix
+++ modules/programs/cli/lazygit/default.nix
@@ -17,9 +17,16 @@ in {
       programs.lazygit = {
         enable = true;
         settings = {
-          gui = fromYAML (
-            pkgs.catppuccin + "/lazygit/themes/blue.yml"
-          );
+          gui = fromYAML (pkgs.fetchFromGitHub {
+              owner = "catppuccin";
+              repo = "lazygit";
+              rev = "d3c95a67ea3f778f7705d8ef814f87ac5213436d";
+              sha256 = "01vhir6243k9wfvlgadv7wsc2s9yb92l67piqsl1dm6kwlhshr3g";
+            }
+            + "/themes/mocha/blue.yml");
+          # gui = fromYAML (
+          #   pkgs.catppuccin + "/lazygit/themes/blue.yml"
+          # );
           git = {
             overrideGpg = true;
           };
