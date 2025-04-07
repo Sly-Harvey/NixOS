d416836a98d5c6d4b2943a79b8adb375c9d9bdea
Sly-Harvey <harveyjacobs64@gmail.com>
2025-04-07 21:11:26 +0100

    Fixed some problems with rofi.sh

--- modules/desktop/hyprland/scripts/rofi.sh
+++ modules/desktop/hyprland/scripts/rofi.sh
@@ -1,37 +1,37 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 
 case $1 in
 drun)
   # rofi_theme="$XDG_CONFIG_HOME/rofi/launchers/type-4/style-7.rasi"
   # rofi_theme="$XDG_CONFIG_HOME/rofi/launchers/type-4/style-3.rasi"
-  rofi_theme="$XDG_CONFIG_HOME/rofi/launchers/type-2/style-2.rasi"
+  rofi_theme="${XDG_CONFIG_HOME:-$HOME/.config}/rofi/launchers/type-2/style-2.rasi"
   r_override="entry{placeholder:'Search Applications...';}listview{lines:9;}"
 
   pkill rofi || rofi -show drun -theme-str "$r_override" -theme "$rofi_theme"
   ;;
 window)
-  rofi_theme="$XDG_CONFIG_HOME/rofi/launchers/type-2/style-2.rasi"
+  rofi_theme="${XDG_CONFIG_HOME:-$HOME/.config}/rofi/launchers/type-2/style-2.rasi"
   r_override="entry{placeholder:'Search Windows...';}listview{lines:9;}"
 
   pkill rofi || rofi -show window -theme-str "$r_override" -theme "$rofi_theme"
   ;;
 file)
-  rofi_theme="$XDG_CONFIG_HOME/rofi/launchers/type-2/style-2.rasi"
+  rofi_theme="${XDG_CONFIG_HOME:-$HOME/.config}/rofi/launchers/type-2/style-2.rasi"
   r_override="entry{placeholder:'Search Files...';}listview{lines:8;}"
 
   pkill rofi || rofi -show filebrowser -theme-str "$r_override" -theme "$rofi_theme"
   ;;
 emoji)
   r_override="entry{placeholder:'Search Emojis...';}listview{lines:15;}"
-  RofiConf="${XDG_CONFIG_HOME:-$HOME/.config}/rofi/launchers/type-4/style-4.rasi"
+  rofi_theme="${XDG_CONFIG_HOME:-$HOME/.config}/rofi/launchers/type-4/style-4.rasi"
 
-  pkill rofi || rofi -modi emoji -show emoji -theme "${RofiConf}" -theme-str "$r_override"
+  pkill rofi || rofi -modi emoji -show emoji -theme "${rofi_theme}" -theme-str "$r_override"
   ;;
 games)
   r_override="entry{placeholder:'Search Games...';}listview{lines:15;}"
-  RofiConf="${XDG_CONFIG_HOME:-$HOME/.config}/rofi/launchers/type-1/style-5.rasi"
+  rofi_theme="${XDG_CONFIG_HOME:-$HOME/.config}/rofi/launchers/type-1/style-5.rasi"
 
-  pkill rofi || rofi -show games -modi games -theme "${RofiConf}" -theme-str "$r_override"
+  pkill rofi || rofi -show games -modi games -theme "${rofi_theme}" -theme-str "$r_override"
   ;;
 help | --help)
   echo "Usage: launch.sh [ACTION]"
