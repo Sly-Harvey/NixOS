1a4ecda47d3f9bd7411ba9045a380718c0d293cc
Sly-Harvey <86175780+Sly-Harvey@users.noreply.github.com>
2025-04-14 09:44:53 +0100

    add helpful message to rebuild
    also changed some drive configs

--- modules/hardware/drives/games.nix
+++ modules/hardware/drives/games.nix
@@ -1,18 +1,14 @@
-{...}: {
-  fileSystems."/mnt/games" = {
+{lib, ...}: {
+  fileSystems."/mnt/games" = lib.mkForce {
     device = "/dev/disk/by-uuid/01DA12C1CBDE9100";
-    fsType = "lowntfs-3g";
+    fsType = "ntfs-3g";
     options = [
-      "X-mount.mkdir"
+      "rw"
       "uid=1000"
       "gid=100"
       "noatime"
-      "rw"
-      "user"
-      "exec"
       "umask=000"
       "nofail"
-      # "async"
       "x-gvfs-show"
       "x-systemd.mount-timeout=5"
     ];
