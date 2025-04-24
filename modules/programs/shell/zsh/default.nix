abfd00f9779cb19ee379a8d137d757bed465e57d
Sly-Harvey <harveyjacobs64@gmail.com>
2025-04-24 08:56:49 +0100

    Updated flake

--- modules/programs/shell/zsh/default.nix
+++ modules/programs/shell/zsh/default.nix
@@ -34,7 +34,7 @@
             "z"
           ];
         };
-        initExtra = ''
+        initContent = ''
           # Powerlevel10k Zsh theme
           source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
           test -f ~/.config/zsh/.p10k.zsh && source ~/.config/zsh/.p10k.zsh
