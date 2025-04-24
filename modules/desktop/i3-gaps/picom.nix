d4dbb52222dd5b4eb80852b9b6f24bb26bbdae53
Sly-Harvey <86175780+Sly-Harvey@users.noreply.github.com>
2025-04-24 07:19:42 +0100

    Using picom for i3

---
+++ modules/desktop/i3-gaps/picom.nix
@@ -0,0 +1,59 @@
+{
+  services.picom = {
+    enable = true;
+    settings = {
+      backend = "glx";
+      # glx-no-stencil = true;
+      use-damage = true;
+      vsync = true;
+      refresh-rate = 0;
+      detect-client-opacity = true;
+
+      inactive-opacity = 0.7;
+      active-opacity = 0.95;
+      frame-opacity = 0.9;
+      inactive-opacity-override = false;
+
+      # Blur
+      blur-background = true;
+      blur = {
+        method = "dual_kawase";
+        strength = 3;
+        # size = 3; # Required for gaussian and box blur
+      };
+
+      # blur-background-exclude = [
+      #   "window_type = 'dock'"
+      # ];
+
+      # corner-radius = 5;
+      # round-borders = 1;
+
+      # Fading
+      fading = false;
+      fade-delta = 3;
+      no-fading-openclose = false;
+
+      # Dim inactive windows. (0.0 - 1.0)
+      inactive-dim = 0.03;
+
+      # Do not let dimness adjust based on window opacity.
+      inactive-dim-fixed = true;
+
+      # Do not let blur radius adjust based on window opacity.
+      blur-background-fixed = true;
+
+      # Can use name or class_g here
+      opacity-rule = [
+        "100:class_g = 'floorp'"
+        "100:class_g = 'firefox'"
+        "100:class_g = 'zen'"
+        "100:class_g = 'Brave-browser'"
+        "90:class_g = 'kitty' && focused"
+        "90:class_g = 'Alacritty' && focused"
+        "90:class_g = 'org.wezfurlong.wezterm' && focused"
+        "80:name = 'Spotify' && !focused"
+      ];
+    };
+  };
+}
