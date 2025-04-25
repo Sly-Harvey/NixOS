9f312fa949981c6531c2e371dfe8be24408801c6
Sly-Harvey <harveyjacobs64@gmail.com>
2025-04-25 21:28:11 +0100

    Updated polybar style

--- modules/desktop/i3-gaps/polybar/scripts/polybar.sh
+++ modules/desktop/i3-gaps/polybar/scripts/polybar.sh
@@ -1,7 +1,7 @@
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload toph &
+    MONITOR=$m polybar --reload main &
   done
 else
-  polybar --reload toph &
+  polybar --reload main &
 fi
