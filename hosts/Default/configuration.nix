1323024a9080a47aa0e9165c700dc6dcf59afd79
Sly-Harvey <harveyjacobs64@gmail.com>
2025-05-06 15:42:19 +0100

    Disabled unused apps and removed problematic filter
    You must disable the custom filter in ublock once for it to be removed

--- hosts/Default/configuration.nix
+++ hosts/Default/configuration.nix
@@ -35,13 +35,13 @@
     ../../modules/programs/media/discord
     ../../modules/programs/media/spicetify
     # ../../modules/programs/media/youtube-music
-    ../../modules/programs/media/thunderbird
+    # ../../modules/programs/media/thunderbird
     # ../../modules/programs/media/obs-studio
     ../../modules/programs/media/mpv
     ../../modules/programs/misc/tlp
     ../../modules/programs/misc/thunar
     # ../../modules/programs/misc/nix-ld
-    ../../modules/programs/misc/virt-manager
+    # ../../modules/programs/misc/virt-manager
     # ../../modules/programs/misc/lact # gpu power and fan control (WIP)
   ];
 
