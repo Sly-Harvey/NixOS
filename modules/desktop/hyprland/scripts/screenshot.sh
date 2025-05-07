41cfe545372803dfc923451799a01da08a357856
Sly-Harvey <harveyjacobs64@gmail.com>
2025-05-07 19:16:54 +0100

    Fixed screenshot.sh

--- modules/desktop/hyprland/scripts/screenshot.sh
+++ modules/desktop/hyprland/scripts/screenshot.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+swpy_dir="${XDG_CONFIG_HOME:-$HOME/.config}/swappy"
 XDG_PICTURES_DIR="${XDG_PICTURES_DIR:-$HOME/Pictures}"
 save_dir="${XDG_PICTURES_DIR}/Screenshots"
 save_file="$(date +'%y%m%d_%Hh%Mm%Ss_screenshot.png')"
+
+mkdir -p $save_dir
+mkdir -p $swpy_dir
+echo -e "[Default]\nsave_dir=$save_dir\nsave_filename_format=$save_file" > $swpy_dir/config
 
 # Ensure required binaries
 command -v grimblast >/dev/null 2>&1 || { echo "grimblast not found"; exit 1; }
