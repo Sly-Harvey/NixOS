de27b163a1890e475c0e2c6256d8151cd02276d2
Sly-Harvey <harveyjacobs64@gmail.com>
2025-05-28 15:34:52 +0100

    Added lact for gpu configuration
    Updated from 0.6.0 to 0.7.4

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,6 +6,5 @@
   # these will be overlayed in nixpkgs automatically.
   # for example: environment.systemPackages = with pkgs; [pokego];
   pokego = pkgs.callPackage ./pokego.nix {};
-  lact = pkgs.callPackage ./lact.nix {};
   sddm-astronaut = pkgs.callPackage ./sddm-themes/astronaut.nix {theme = settings.sddmTheme;};
 }
