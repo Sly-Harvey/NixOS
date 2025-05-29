28bd73f5d0a3eb2a2a94aab1ce7699b3666a3d08
Sly-Harvey <86175780+Sly-Harvey@users.noreply.github.com>
2025-05-29 19:51:53 +0100

    Updated nvk config

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -511,8 +511,8 @@
           monitor=,preferred,auto,1
 
           # 1080p-HDR monitor on the left, 4K-HDR monitor in the middle and 1080p vertical monitor on the right.
-          monitor=desc:BNQ BenQ EW277HDR 99J01861SL0,preferred,-1920x0,1,bitdepth,8
-          monitor=desc:BNQ BenQ EL2870U PCK00489SL0,3840x2160@60,0x0,2,bitdepth,10
+          monitor=desc:BNQ BenQ EW277HDR 99J01861SL0,preferred,-1920x0,1
+          monitor=desc:BNQ BenQ EL2870U PCK00489SL0,preferred,0x0,2
           monitor=desc:BNQ BenQ xl2420t 99D06760SL0,preferred,1920x0,1,transform,1 # 5 for fipped
 
           # Binds workspaces to my monitors only (find desc with: hyprctl monitors)
