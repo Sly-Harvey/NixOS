c3ee51c89a0e76af97930a5f114195e889eb3df4
Sly-Harvey <86175780+Sly-Harvey@users.noreply.github.com>
2025-07-28 23:10:42 +0100

    Fixed and updated firefox config

---
+++ modules/programs/browser/firefox/policies.nix
@@ -0,0 +1,273 @@
+{lib, ...}: {
+  AllowFileSelectionDialogs = true;
+  AppAutoUpdate = false;
+  AutofillAddressEnabled = false;
+  AutofillCreditCardEnabled = false;
+  #AutoLaunchProtocolsFromOrigins = { };
+  BackgroundAppUpdate = false;
+  BlockAboutAddons = false;
+  BlockAboutConfig = false;
+  BlockAboutProfiles = false;
+  BlockAboutSupport = false;
+  #Containers = { };
+  DisableAppUpdate = true;
+  DisableFirefoxAccounts = true;
+  DisableFirefoxScreenshots = true;
+  DisableFirefoxStudies = true;
+  DisableFormHistory = true;
+  DisableMasterPasswordCreation = true;
+  DisablePocket = true;
+  DisablePrivateBrowsing = false;
+  DisableProfileImport = false;
+  DisableProfileRefresh = false;
+  DisableSafeMode = false;
+  DisableTelemetry = true;
+  DNSOverHTTPS = {
+    Enabled = true;
+  };
+  EnableTrackingProtection = {
+    Value = true;
+    Locked = true;
+    Cryptomining = true;
+    Fingerprinting = true;
+  };
+  EncryptedMediaExtensions = {
+    Enabled = true;
+  };
+  ExtensionUpdate = true;
+  FirefoxHome = {
+    Search = false;
+    TopSites = false;
+    SponsoredTopSites = false;
+    Highlights = false;
+    Pocket = false;
+    SponsoredPocket = false;
+    Snippets = false;
+    Locked = false;
+  };
+  HardwareAcceleration = true;
+  ManualAppUpdateOnly = true;
+  NoDefaultBookmarks = false;
+  OfferToSaveLogins = false;
+  PasswordManagerEnabled = false;
+  PictureInPicture = {
+    Enabled = true;
+  };
+  PopupBlocking = {
+    Allow = [];
+    Default = true;
+  };
+  Preferences = {
+    "browser.tabs.warnOnClose" = {
+      Value = false;
+    };
+  };
+  PromptForDownloadLocation = true;
+  SearchSuggestEnabled = false;
+  ShowHomeButton = false;
+  StartDownloadsInTempDirectory = false;
+  UserMessaging = {
+    ExtensionRecommendations = false;
+    SkipOnboarding = true;
+  };
+  ExtensionSettings = {
+    "*" = {
+      blocked_install_message = "Addon is not added in the nix config";
+      installation_mode = "blocked";
+    };
+    "uBlock0@raymondhill.net" = {
+      private_browsing = true;
+      default_area = "navbar";
+      installation_mode = "force_installed";
+      install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+    };
+    "firemonkey@eros.man" = {
+      private_browsing = true;
+      default_area = "navbar";
+      installation_mode = "force_installed";
+      install_url = "https://addons.mozilla.org/firefox/downloads/latest/firemonkey/latest.xpi";
+    };
+    "extension@one-tab.com" = {
+      private_browsing = false;
+      default_area = "navbar";
+      installation_mode = "force_installed";
+      install_url = "https://addons.mozilla.org/firefox/downloads/latest/onetab/latest.xpi";
+    };
+    "{c4b582ec-4343-438c-bda2-2f691c16c262}" = {
+      private_browsing = true;
+      default_area = "navbar";
+      installation_mode = "force_installed";
+      install_url = "https://addons.mozilla.org/firefox/downloads/latest/600-sound-volume/latest.xpi";
+    };
+    "addon@darkreader.org" = {
+      private_browsing = true;
+      # default_area = "navbar";
+      installation_mode = "force_installed";
+      install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
+    };
+    "sponsorBlocker@ajay.app" = {
+      private_browsing = true;
+      default_area = "menupanel";
+      installation_mode = "force_installed";
+      install_url = "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi";
+    };
+    "{762f9885-5a13-4abd-9c77-433dcd38b8fd}" = {
+      private_browsing = true;
+      installation_mode = "force_installed";
+      install_url = "https://addons.mozilla.org/firefox/downloads/latest/return-youtube-dislikes/latest.xpi";
+    };
+    "frankerfacez@frankerfacez.com" = {
+      private_browsing = true;
+      installation_mode = "force_installed";
+      install_url = "https://addons.mozilla.org/firefox/downloads/latest/frankerfacez/latest.xpi";
+    };
+    # Catppuccin theme
+    "{8446b178-c865-4f5c-8ccc-1d7887811ae3}" = {
+      private_browsing = true;
+      installation_mode = "force_installed";
+      install_url = "https://addons.mozilla.org/firefox/downloads/latest/catppuccin-mocha-lavender-git/latest.xpi";
+    };
+    # View Xpi Id's in Firefox Extension Store
+    "queryamoid@kaply.com" = {
+      private_browsing = true;
+      installation_mode = "force_installed";
+      install_url = "https://github.com/mkaply/queryamoid/releases/download/v0.2/query_amo_addon_id-0.2-fx.xpi";
+    };
+  };
+  "3rdparty".Extensions = {
+    "addon@darkreader.org" = {
+      enabled = true;
+      automation = {
+        enabled = true;
+        behavior = "OnOff";
+        mode = "system";
+      };
+      detectDarkTheme = true;
+      enabledByDefault = true;
+      changeBrowserTheme = false;
+      enableForProtectedPages = true;
+      fetchNews = true;
+      syncSitesFixes = true;
+      previewNewDesign = true;
+      # previewNewestDesign = true; # TODO: test
+
+      # Catppuccin mocha theme
+      /* theme = {
+        mode = 1;
+        brightness = 100;
+        contrast = 100;
+        grayscale = 0;
+        sepia = 0;
+        useFont = false;
+        fontFamily = "Open Sans";
+        textStroke = 0;
+        engine = "dynamicTheme";
+        stylesheet = "";
+        darkSchemeBackgroundColor = "#1e1e2e";
+        darkSchemeTextColor = "#cdd6f4";
+        lightSchemeBackgroundColor = "#eff1f5";
+        lightSchemeTextColor = "#4c4f69";
+        scrollbarColor = "";
+        selectionColor = "#585b70"; # For the light scheme: #acb0be
+        styleSystemControls = true;
+        lightColorScheme = "Default";
+        darkColorScheme = "Default";
+        immediateModify = false;
+      }; */
+
+      # enabledFor = [];
+      # disabledFor = [];
+    };
+    "uBlock0@raymondhill.net" = {
+      advancedSettings = [
+        [
+          "userResourcesLocation"
+          "https://raw.githubusercontent.com/pixeltris/TwitchAdSolutions/master/video-swap-new/video-swap-new-ublock-origin.js"
+        ]
+      ];
+      adminSettings = {
+        userFilters = lib.concatMapStrings (x: x + "\n") [
+          "twitch.tv##+js(twitch-videoad)"
+          "||1337x.vpnonly.site"
+        ];
+        userSettings = rec {
+          uiTheme = "dark";
+          uiAccentCustom = true;
+          uiAccentCustom0 = "#CA9EE6";
+          cloudStorageEnabled = lib.mkForce false; # Security liability?
+          advancedUserEnabled = true;
+          userFiltersTrusted = true;
+          importedLists = [
+            "https://raw.githubusercontent.com/reek/anti-adblock-killer/master/anti-adblock-killer-filters.txt"
+            "https://easylist-downloads.adblockplus.org/antiadblockfilters.txt"
+            "https://gitflic.ru/project/magnolia1234/bypass-paywalls-clean-filters/blob/raw?file=bpc-paywall-filter.txt"
+            "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/BrowseWebsitesWithoutLoggingIn.txt"
+            "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/ClearURLs for uBo/clear_urls_uboified.txt"
+            "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Dandelion Sprout's Anti-Malware List.txt"
+            "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/LegitimateURLShortener.txt"
+            # "https://raw.githubusercontent.com/OsborneLabs/Columbia/master/Columbia.txt"
+            "https://raw.githubusercontent.com/bogachenko/fuckfuckadblock/master/fuckfuckadblock.txt?_=rawlist"
+            "https://raw.githubusercontent.com/iam-py-test/my_filters_001/main/antimalware.txt"
+            "https://raw.githubusercontent.com/liamengland1/miscfilters/master/antipaywall.txt"
+            "https://raw.githubusercontent.com/yokoffing/filterlists/main/annoyance_list.txt"
+            "https://raw.githubusercontent.com/yokoffing/filterlists/main/privacy_essentials.txt"
+          ];
+          externalLists = lib.concatStringsSep "\n" importedLists;
+          popupPanelSections = 31;
+        };
+        selectedFilterLists = [
+          "ublock-filters"
+          "ublock-badware"
+          "ublock-privacy"
+          "ublock-quick-fixes"
+          "ublock-unbreak"
+          "easylist"
+          "adguard-generic"
+          "adguard-mobile"
+          "easyprivacy"
+          "adguard-spyware"
+          "adguard-spyware-url"
+          "block-lan"
+          "urlhaus-1"
+          "curben-phishing"
+          "plowe-0"
+          "dpollock-0"
+          "fanboy-cookiemonster"
+          "ublock-cookies-easylist"
+          "adguard-cookies"
+          "ublock-cookies-adguard"
+          "fanboy-social"
+          "adguard-social"
+          "fanboy-thirdparty_social"
+          "easylist-chat"
+          "easylist-newsletters"
+          "easylist-notifications"
+          "easylist-annoyances"
+          "adguard-mobile-app-banners"
+          "adguard-other-annoyances"
+          "adguard-popup-overlays"
+          "adguard-widgets"
+          "ublock-annoyances"
+          "DEU-0"
+          "FRA-0"
+          "NLD-0"
+          "RUS-0"
+          "https://raw.githubusercontent.com/reek/anti-adblock-killer/master/anti-adblock-killer-filters.txt"
+          "https://easylist-downloads.adblockplus.org/antiadblockfilters.txt"
+          "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Dandelion Sprout's Anti-Malware List.txt"
+          "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/LegitimateURLShortener.txt"
+          "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/BrowseWebsitesWithoutLoggingIn.txt"
+          "https://raw.githubusercontent.com/yokoffing/filterlists/main/privacy_essentials.txt"
+          "https://raw.githubusercontent.com/yokoffing/filterlists/main/annoyance_list.txt"
+          "https://raw.githubusercontent.com/liamengland1/miscfilters/master/antipaywall.txt"
+          "https://gitflic.ru/project/magnolia1234/bypass-paywalls-clean-filters/blob/raw?file=bpc-paywall-filter.txt"
+          "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/ClearURLs for uBo/clear_urls_uboified.txt"
+          "https://raw.githubusercontent.com/iam-py-test/my_filters_001/main/antimalware.txt"
+          # "https://raw.githubusercontent.com/OsborneLabs/Columbia/master/Columbia.txt"
+          "https://raw.githubusercontent.com/bogachenko/fuckfuckadblock/master/fuckfuckadblock.txt?_=rawlist"
+          "user-filters"
+        ];
+      };
+    };
+  };
+}
---
+++ modules/programs/browser/firefox/search.nix
@@ -0,0 +1,136 @@
+{pkgs, ...}: {
+  force = true;
+  default = "google";
+  privateDefault = "Startpage";
+  order = [
+    "Startpage"
+    "Searx"
+    "Brave"
+    "NixOS Packages"
+    "NixOS Options"
+    "NixOS Wiki"
+    "Home Manager Options"
+    "google"
+  ];
+  engines = let
+    icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+  in {
+    "Startpage" = {
+      urls = [
+        {
+          template = "https://www.startpage.com/sp/search?query={searchTerms}&prfe=c602752472dd4a3d8286a7ce441403da08e5c4656092384ed3091a946a5a4a4c99962d0935b509f2866ff1fdeaa3c33a007d4d26e89149869f2f7d0bdfdb1b51aa7ae7f5f17ff4a233ff313d";
+        }
+      ];
+      # icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+      icon = "https://www.startpage.com/sp/cdn/favicons/favicon-gradient.ico";
+      definedAliases = ["@sp"];
+      updateInterval = 24 * 60 * 60 * 1000;
+    };
+    "Brave" = {
+      urls = [
+        {
+          template = "https://search.brave.com/search";
+          params = [
+            {
+              name = "q";
+              value = "{searchTerms}";
+            }
+          ];
+        }
+      ];
+      icon = "https://brave.com/static-assets/images/brave-logo-sans-text.svg";
+      definedAliases = ["@br"];
+      updateInterval = 24 * 60 * 60 * 1000;
+    };
+    "Searx" = {
+      urls = [{template = "https://searx.aicampground.com/?q={searchTerms}";}];
+      # icon = "https://nixos.wiki/favicon.png";
+      icon = builtins.fetchurl {
+        url = "https://raw.githubusercontent.com/searxng/searxng/edf6d96625444b5b214b4ca0e2885467ed265411/src/brand/searxng-wordmark.svg";
+        sha256 = "sha256:0lnc0cf7rgl6a54zm4i5z3i3npp87bg9kmwf5mii88gys980y32g";
+      };
+      definedAliases = ["@sx"];
+    };
+    "NixOS Packages" = {
+      inherit icon;
+      urls = [
+        {
+          template = "https://search.nixos.org/packages";
+          params = [
+            {
+              name = "type";
+              value = "packages";
+            }
+            {
+              name = "query";
+              value = "{searchTerms}";
+            }
+          ];
+        }
+      ];
+      definedAliases = ["@np" "@nixpkgs"];
+    };
+    "NixOS Options" = {
+      inherit icon;
+      urls = [
+        {
+          template = "https://search.nixos.org/options";
+          params = [
+            {
+              name = "type";
+              value = "packages";
+            }
+            {
+              name = "query";
+              value = "{searchTerms}";
+            }
+          ];
+        }
+      ];
+      definedAliases = ["@no" "@nixopts"];
+    };
+    "NixOS Wiki" = {
+      inherit icon;
+      urls = [{template = "https://nixos.wiki/index.php?search={searchTerms}";}];
+      updateInterval = 24 * 60 * 60 * 1000; # every day
+      definedAliases = ["@nw"];
+    };
+    "Home Manager" = {
+      inherit icon;
+      urls = [{template = "https://home-manager-options.extranix.com/?query={searchTerms}";}];
+      # urls = [
+      #   {
+      #     template = "https://mipmip.github.io/home-manager-option-search";
+      #     params = [
+      #       {
+      #         name = "query";
+      #         value = "{searchTerms}";
+      #       }
+      #     ];
+      #   }
+      # ];
+      definedAliases = ["@hm" "@home" "'homeman"];
+    };
+    "My NixOS" = {
+      inherit icon;
+      urls = [{template = "https://mynixos.com/search?q={searchTerms}";}];
+      definedAliases = [
+        "@mn"
+        "@nx"
+        "@mynixos"
+      ];
+    };
+    "Noogle" = {
+      inherit icon;
+      urls = [{template = "https://noogle.dev/q?term={searchTerms}";}];
+      updateInterval = 24 * 60 * 60 * 1000;
+      definedAliases = [
+        "@noogle"
+        "@ng"
+      ];
+    };
+    "bing".metaData.hidden = true;
+    "Ebay".metaData.hidden = true;
+    "google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias
+  };
+}
