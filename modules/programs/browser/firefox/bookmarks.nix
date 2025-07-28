ddc108a6b9482786f7f7ba5425b980c43f319c6e
Sly-Harvey <harveyjacobs64@gmail.com>
2025-07-28 23:10:42 +0100

    Fixed and updated firefox config

---
+++ modules/programs/browser/firefox/bookmarks.nix
@@ -0,0 +1,114 @@
+{
+  force = true;
+  settings = [
+    {
+      name = "Bookmarks Toolbar";
+      toolbar = true;
+      bookmarks = [
+        {
+          name = "Youtube";
+          url = "https://www.youtube.com";
+        }
+        {
+          name = "Twitch";
+          url = "https://www.twitch.tv";
+        }
+        {
+          name = "Netflix";
+          url = "https://www.netflix.com";
+        }
+        {
+          name = "Github";
+          url = "https://github.com/";
+        }
+        {
+          name = "ChatGPT";
+          url = "https://chatgpt.com/";
+        }
+        {
+          name = "Physics";
+          bookmarks = [
+            {
+              name = "Qubits";
+              url = "https://arxiv.org/pdf/1904.06560";
+            }
+            {
+              name = "Multi-Qubits";
+              url = "https://arxiv.org/pdf/2502.16479";
+            }
+            {
+              name = "Cryogenic Modeling";
+              url = "https://arxiv.org/pdf/2502.01945";
+            }
+            {
+              name = "Nuclear Spin Engineering";
+              url = "https://arxiv.org/pdf/2502.18450";
+            }
+            {
+              name = "Mixed State Entanglement";
+              url = "https://arxiv.org/pdf/2502.18446";
+            }
+          ];
+        }
+        {
+          name = "NixOS";
+          bookmarks = [
+            {
+              name = "Search NixOS";
+              url = "https://mynixos.com/";
+            }
+            {
+              name = "NixOS Wiki";
+              url = "https://wiki.nixos.org/wiki/NixOS_Wiki";
+            }
+            {
+              name = "NixOS Packages";
+              url = "https://search.nixos.org/packages";
+            }
+            {
+              name = "NixOS Options";
+              url = "https://search.nixos.org/options";
+            }
+            {
+              name = "NixOS Configs";
+              url = "https://wiki.nixos.org/wiki/Configuration_Collection";
+            }
+            {
+              name = "Nix Docs";
+              url = "https://noogle.dev/";
+            }
+            {
+              name = "Learn Nix";
+              url = "https://nix.dev/";
+            }
+            {
+              name = "NixOS & Flakes Book";
+              url = "https://nixos-and-flakes.thiscute.world/";
+            }
+            {
+              name = "Interactive Nix Lessons";
+              url = "https://nixcloud.io/tour/?id=introduction/nix";
+            }
+            {
+              name = "Nixpkgs Issue Tracker";
+              url = "https://nixpkgs-tracker.ocfox.me/";
+            }
+          ];
+        }
+        {
+          name = "Search Engines";
+          bookmarks = [
+            {
+              name = "Startpage";
+              url = "https://www.startpage.com/do/mypage.pl?prfe=c602752472dd4a3d8286a7ce441403da08e5c4656092384ed3091a946a5a4a4c99962d0935b509f2866ff1fdeaa3c33a007d4d26e89149869f2f7d0bdfdb1b51aa7ae7f5f17ff4a233ff313d";
+            }
+            {
+              name = "SearX";
+              url = "https://searx.aicampground.com";
+            }
+          ];
+        }
+      ];
+    }
+  ];
+}
