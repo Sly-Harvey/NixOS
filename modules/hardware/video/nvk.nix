3a6c23e98ea53854853174d3072ec6b7a2f0c995
Sly-Harvey <86175780+Sly-Harvey@users.noreply.github.com>
2025-07-29 01:42:17 +0100

    Updated nvk config

--- modules/hardware/video/nvk.nix
+++ modules/hardware/video/nvk.nix
@@ -1,27 +1,21 @@
 # This module uses nouveau with NVK which is the nvidia open-source user-space driver and is not recommended to use as of 30/05/24 since it's unstable
 {pkgs, lib, ...}: let
   env = {
-    NVK_I_WANT_A_BROKEN_VULKAN_DRIVER = "1"; # Adds support for my gpu (gtx 1080)
-    # NOUVEAU_USE_ZINK = 1; # TODO: TEST
-    MESA_LOADER_DRIVER_OVERRIDE = "zink"; # TODO TEST
+    NVK_I_WANT_A_BROKEN_VULKAN_DRIVER = "1"; # Adds support for older gpus
+    MESA_LOADER_DRIVER_OVERRIDE = "zink";
     GALLIUM_DRIVER = "zink";
-    # MESA_VK_VERSION_OVERRIDE = "1.3";
-    # __GLX_VENDOR_LIBRARY_NAME = "mesa";
-    # MESA_VK_DEVICE_SELECT="nvk";
 
     GBM_BACKEND = "nouveau";
     WLR_RENDERER = "vulkan";
-    WLR_DRM_NO_ATOMIC = "1";
     WLR_NO_HARDWARE_CURSORS = "1";
     __GL_GSYNC_ALLOWED = "1"; # GSync
   };
 in {
   boot = {
     kernelParams = [
+      "nouveau.modeset=1"
       "nouveau.config=NvGspRm=1"
-      "nouveau.config=NvModesetKms=0" # TODO Test
-      "nouveau.debug=info,VBIOS=info,gsp=debug" # TODO Remove
-      # "nouveau.modeset=1"
+      # "nouveau.debug=info,VBIOS=info,gsp=debug" # TODO Remove
     ];
     kernelModules = ["nouveau"];
     blacklistedKernelModules = ["nvidia" "nvidia_uvm"];
