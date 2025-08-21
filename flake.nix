3ea0f070cf0df3b6db71c08007b5acd0b07740f9
storytyler <161856107+storytyler@users.noreply.github.com>
2025-08-21 07:36:02 +0000

    Update flake.nix
    Adjusted presets to avoid repetition

--- flake.nix
+++ flake.nix
@@ -47,27 +47,27 @@
     inherit (self) outputs;
     settings = {
       # User configuration
-      username = "zer0"; # automatically set with install.sh and live-install.sh
-      editor = "nixvim"; # nixvim, vscode, nvchad, neovim, emacs (WIP)
-      browser = "firefox"; # firefox, floorp, zen
-      terminal = "kitty"; # kitty, alacritty, wezterm
+      username = "player00"; # automatically set with install.sh and live-install.sh
+      editor = "vscode"; # nixvim, vscode, nvchad, neovim, emacs (WIP)
+      browser = "zen"; # firefox, floorp, zen
+      terminal = "alacritty"; # kitty, alacritty, wezterm
       terminalFileManager = "yazi"; # yazi or lf
-      sddmTheme = "purple_leaves"; # astronaut, black_hole, purple_leaves, jake_the_dog, hyprland_kath
-      wallpaper = "kurzgesagt"; # see modules/themes/wallpapers
+      sddmTheme = "tokyo-night"; # astronaut, black_hole, purple_leaves, jake_the_dog, hyprland_kath
+      wallpaper = "dark-forest"; # see modules/themes/wallpapers
 
       # System configuration
-      videoDriver = "nvidia"; # CHOOSE YOUR GPU DRIVERS (nvidia, amdgpu or intel)
-      hostname = "NixOS"; # CHOOSE A HOSTNAME HERE
-      locale = "en_GB.UTF-8"; # CHOOSE YOUR LOCALE
-      timezone = "Europe/London"; # CHOOSE YOUR TIMEZONE
-      kbdLayout = "gb"; # CHOOSE YOUR KEYBOARD LAYOUT
-      kbdVariant = "extd"; # CHOOSE YOUR KEYBOARD VARIANT (Can leave empty)
-      consoleKeymap = "uk"; # CHOOSE YOUR CONSOLE KEYMAP (Affects the tty?)
+      videoDriver = "intel"; # CHOOSE YOUR GPU DRIVERS (nvidia, amdgpu or intel)
+      hostname = "Station-00"; # CHOOSE A HOSTNAME HERE
+      locale = "en_US.UTF-8"; # CHOOSE YOUR LOCALE
+      timezone = "Chicago"; # CHOOSE YOUR TIMEZONE
+      kbdLayout = "us"; # CHOOSE YOUR KEYBOARD LAYOUT
+      kbdVariant = ""; # CHOOSE YOUR KEYBOARD VARIANT (Can leave empty)
+      consoleKeymap = "us"; # CHOOSE YOUR CONSOLE KEYMAP (Affects the tty?)
     };
 
     systems = [
       "x86_64-linux"
-      "aarch64-linux"
+
     ];
     forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
