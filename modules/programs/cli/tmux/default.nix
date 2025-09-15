d6bb30a77aff843180a319d7f66bd847eb149be5
Sly-Harvey <86175780+Sly-Harvey@users.noreply.github.com>
2025-09-15 13:05:31 +0100

    Added more tmux split binds

--- modules/programs/cli/tmux/default.nix
+++ modules/programs/cli/tmux/default.nix
@@ -102,7 +102,9 @@ in {
 
           # Splits
           bind | split-window -h -c "#{pane_current_path}"
+          bind [ split-window -h -c "#{pane_current_path}"
           bind - split-window -v -c "#{pane_current_path}"
+          bind ] split-window -v -c "#{pane_current_path}"
           bind c new-window -c "#{pane_current_path}"
 
           # Select windows
