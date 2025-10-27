b10ca33ef6493be4146b1e36b04ee193e134b9df
Sly-Harvey <86175780+Sly-Harvey@users.noreply.github.com>
2025-10-27 04:48:29 +0000

    Show image previews in clipboard manager

--- modules/desktop/hyprland/scripts/ClipManager.sh
+++ modules/desktop/hyprland/scripts/ClipManager.sh
@@ -1,20 +1,30 @@
 #!/usr/bin/env bash
 # Clipboard Manager. This script uses cliphist, rofi, and wl-copy.
 
-# Actions:
-# CTRL Del to delete an entry
-# ALT Del to wipe clipboard contents
+tmp_dir="/tmp/cliphist_rofi_previews"
 
-rofi_theme="${XDG_CONFIG_HOME:-$HOME/.config}/rofi/launchers/type-1/style-6.rasi"
-r_override="entry{placeholder:'Search Clipboard...';}listview{lines:9;}"
+trap 'rm -rf "$tmp_dir"' EXIT
+
+mkdir -p "$tmp_dir"
+
+read -r -d '' gawk_prog <<EOF
+/^[0-9]+\s<meta http-equiv=/ { next }
+match(\$0, /^([0-9]+)\s(\[\[\s)?binary.*(jpg|jpeg|png|bmp)/, grp) {
+    system("cliphist decode " grp[1] " > " tmp_dir "/" grp[1] "." grp[3])
+    print \$0"\0icon\x1f"tmp_dir"/"grp[1]"."grp[3]
+    next
+}
+1
+EOF
+# ---------------------------------------------
 
 while true; do
   result=$(
-    rofi -dmenu -i \
+    rofi -dmenu \
       -kb-custom-1 "Control-Delete" \
       -kb-custom-2 "Alt-Delete" \
-      -theme-str "$r_override" \
-      -theme "$rofi_theme" < <(cliphist list)
+      -theme "$HOME/.config/rofi/launchers/type-1/style-6.rasi" \
+      < <(cliphist list | gawk -v tmp_dir="$tmp_dir" "$gawk_prog")
   )
 
   case "$?" in
