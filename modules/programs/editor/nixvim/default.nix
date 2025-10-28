54df7737faf8dd462a662b19bb2b2e8b2907d5c5
Sly-Harvey <86175780+Sly-Harvey@users.noreply.github.com>
2025-10-28 05:02:40 +0000

    Extend nixvim with base16

--- modules/programs/editor/nixvim/default.nix
+++ modules/programs/editor/nixvim/default.nix
@@ -4,33 +4,48 @@
   pkgs,
   ...
 }:
-let
-  inherit (import ../../../../hosts/${host}/variables.nix) terminal;
-in
 {
   home-manager.sharedModules = [
-    (_: {
-      home.packages = with pkgs; [
-        inputs.nixvim.packages.${system}.default
-      ];
-      xdg.desktopEntries = {
-        "nvim" = {
-          name = "Neovim wrapper";
-          genericName = "Text Editor";
-          comment = "Edit text files";
-          exec = "${pkgs.${terminal}}/bin/${terminal} --class \"nvim-wrapper\" -e nvim %F";
-          icon = "nvim";
-          mimeType = [
-            "text/plain"
-            "text/x-makefile"
-          ];
-          categories = [
-            "Development"
-            "TextEditor"
-          ];
-          terminal = false; # Important: set to false since we're calling kitty directly
+    (
+      { config, ... }:
+      let
+        inherit (import ../../../../hosts/${host}/variables.nix) terminal;
+        nixvim-package = inputs.nixvim.packages.${pkgs.system}.default;
+
+        extended-nixvim =
+          if config.stylix.targets.nixvim.enable then
+            # For nixos-unstable
+            nixvim-package.extend config.stylix.targets.nixvim.exportedModule
+
+            # for nixos-25.05 (Deprecated by 26.11)
+            # nixvim-package.extend config.lib.stylix.nixvim.config
+          else
+            nixvim-package;
+      in
+      {
+        home.packages = with pkgs; [
+          extended-nixvim
+          # inputs.nixvim.packages.${system}.default
+        ];
+        xdg.desktopEntries = {
+          "nvim" = {
+            name = "Neovim wrapper";
+            genericName = "Text Editor";
+            comment = "Edit text files";
+            exec = "${pkgs.${terminal}}/bin/${terminal} --class \"nvim-wrapper\" -e nvim %F";
+            icon = "nvim";
+            mimeType = [
+              "text/plain"
+              "text/x-makefile"
+            ];
+            categories = [
+              "Development"
+              "TextEditor"
+            ];
+            terminal = false; # Important: set to false since we're calling kitty directly
+          };
         };
-      };
-    })
+      }
+    )
   ];
 }
