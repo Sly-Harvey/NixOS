6d5ffcdb8aa6125bad965d5e3c0d186734d2432e
Sly-Harvey <86175780+Sly-Harvey@users.noreply.github.com>
2025-11-06 21:00:10 +0000

    Removed duplicate definition in amdgpu module

--- modules/hardware/video/amdgpu.nix
+++ modules/hardware/video/amdgpu.nix
@@ -1,14 +1,12 @@
 { pkgs, ... }:
 
 {
-  services.xserver.videoDrivers = [ "amdgpu" ];
   services.xserver = {
     enable = true;
     videoDrivers = [ "amdgpu" ];
   };
   environment.systemPackages = with pkgs; [ rocmPackages.amdsmi ];
-  environment.systemPackages = with pkgs; [ rocmPackages.amdsmi ];
-   hardware.amdgpu = {
-     opencl.enable = true;
-   };
+  hardware.amdgpu = {
+    opencl.enable = true;
+  };
 }
