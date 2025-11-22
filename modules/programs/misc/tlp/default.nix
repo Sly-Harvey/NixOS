e3c27d41ed5820993fc4c870a2a4d702f5ef8aa3
Sly-Harvey <86175780+Sly-Harvey@users.noreply.github.com>
2025-11-22 23:56:07 +0000

    Tweaked tlp

--- modules/programs/misc/tlp/default.nix
+++ modules/programs/misc/tlp/default.nix
@@ -4,15 +4,15 @@
     enable = true;
     settings = {
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave"; # schedutil powersave, ondemand
+      CPU_SCALING_GOVERNOR_ON_BAT = "schedutil"; # schedutil powersave, ondemand
 
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power"; # power, balance_power
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power"; # power, balance_power
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
 
       CPU_MIN_PERF_ON_AC = 0;
       CPU_MAX_PERF_ON_AC = 100;
       CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 50;
+      CPU_MAX_PERF_ON_BAT = 60;
 
       # Protect battery
       START_CHARGE_THRESH_BAT0 = 82;
