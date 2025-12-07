ffdeac048d1bc4f93a4d663bb6269057f016fd22
Sly-Harvey <86175780+Sly-Harvey@users.noreply.github.com>
2025-12-07 11:18:56 +0000

    Updated flake

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
     nix-flatpak.url = "github:gmodena/nix-flatpak?ref=latest";
     home-manager = {
       url = "github:nix-community/home-manager";
