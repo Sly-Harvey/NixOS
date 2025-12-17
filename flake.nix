d06306339efca9b32544b7b93a579cd283572030
Sly-Harvey <86175780+Sly-Harvey@users.noreply.github.com>
2025-12-17 15:55:55 +0000

    Updated flake

--- flake.nix
+++ flake.nix
@@ -49,7 +49,10 @@
     };
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+      };
     };
     nvchad4nix = {
       url = "github:nix-community/nix4nvchad";
