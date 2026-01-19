7f3dd24a114da7ac7e2d25c3300086c1602228f2
Sly-Harvey <86175780+Sly-Harvey@users.noreply.github.com>
2026-01-19 01:07:16 +0000

    Updated nix settings

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -36,24 +36,28 @@ in
   nix = {
     # Nix Package Manager Settings
     settings = {
+      trusted-users = [ "root" "@wheel" ]; # Required by Cachix to be used as non-root user
+      accept-flake-config = true;
       builders-use-substitutes = true;
       download-buffer-size = 200000000;
       auto-optimise-store = true; # May make rebuilds longer but less size
       substituters = [
-        "https://cache.nixos.org" # Can add ?priority=10 and the end
-        "https://nix-community.cachix.org"
-        "https://chaotic-nyx.cachix.org"
-        "https://cachix.cachix.org"
-        "https://nix-gaming.cachix.org"
+        "https://cache.nixos.org/"
+        "https://nix-community.cachix.org/"
+        "https://hyprland.cachix.org/"
+        "https://cuda-maintainers.cachix.org/"
+        # "https://nix-gaming.cachix.org"
+        # "https://chaotic-nyx.cachix.org"
         # "https://nixpkgs-wayland.cachix.org"
         # "https://devenv.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
-        "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
-        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+        # "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+        # "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
         # "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
         # "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       ];
--- modules/hardware/video/nvidia.nix
+++ modules/hardware/video/nvidia.nix
@@ -62,10 +62,4 @@ in
         "nvidia-x11"
       ];
   };
-  nix.settings = {
-    substituters = [ "https://cuda-maintainers.cachix.org" ];
-    trusted-public-keys = [
-      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-    ];
-  };
 }
